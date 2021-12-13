4f0dab264f9e8b7bae7a1fda1ca5253ec248b533
anie6 <90655634+anie6@users.noreply.github.com>
2021-12-12 23:25:01 -0500

    dividing by 0 then result rquals 0.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,7 +17,7 @@ public class SimpleCalculatorTest {
 	}
 	public void testDivide() {
 		SimpleCalculator dcalc = new SimpleCalculator();
-		assertEquals(dcalc.divide(6, 3), 2);
+		assertEquals(dcalc.divide(6, 0), 0);
 	}
 	public void testMultiple() {
 		SimpleCalculator mcalc = new SimpleCalculator();
